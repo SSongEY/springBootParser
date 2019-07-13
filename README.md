d687a7319ea50d7fd703a6fc46597b416d881d98
yang-yeongman <yang-yeongman@a1platform.co.kr>
2019-07-13 23:07:57 +0900

    README 수정

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # springBootParser
+
+**Build & Excute**
+
+​	`gradle build && java -jar build/libs/parser-1.0-SNAPSHOT.jar `
+
+**ROOT Page**
+​	URL : http://localhost:8080
+
+## 기본 요구사항
+
+### 1. 모든 문자 입력 가능
+
+```
+1. 입력 URL의 HTML 값을 Type에 따라 parsing
+```
+### 2. 영어 숫자만 출력
+
+```
+1. 오로지 영어 숫자만 출력한다.
+```
+
+### 3. 오름차순 정렬
+
+```
+1. 영어 : AaBbCcDd....Zz
+2. 숫자 : 0,1,2,3,4,5,6,7,8,9
+```
+
+### 4. 교차 출력
+
+```
+1. 정렬된 상태에서 영어,숫자,영어,숫자 순으로 출
+```
+
+### 5. 출력 묶음 단위
+
+```
+1. 출력 묶음 단위 기준으로 몫의 데이터 값과 나머지 데이터 값으로 출력
+```
+
+## Skill Set
+* Back-end
+
+| 기술 |비고 |
+|:-------------|:-------------|
+| JAVA 8 |-|
+| Spring Boot 2.1.6 | Java-Platform Framework |
+
+* Front-end
+
+| 기술 |비고 |
+|:-------------|:-------------|
+| jquery 3.4 |-|
+| Bootstrap 4.3.1 | Front-end Component Libarary |
+
+
--- src/main/java/com/yym/web/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/yym/web/exception/GlobalControllerExceptionHandler.java
@@ -2,6 +2,8 @@ package com.yym.web.exception;
 
 import com.yym.web.common.ErrorCode;
 import com.yym.web.dto.ApiErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -17,10 +19,12 @@ import javax.servlet.http.HttpServletResponse;
 @ControllerAdvice(basePackages = {"com.yym.web.api"})
 public class GlobalControllerExceptionHandler implements ResponseBodyAdvice<Object> {
 
+  private Logger logger = LoggerFactory.getLogger(getClass());
+
   @ExceptionHandler
   @ResponseBody
   public ApiErrorResponse handleConflict(final Exception e, final HttpServletResponse response) {
-    e.printStackTrace();
+    logger.error(e.getMessage());
     if (e instanceof ApiException) {
       final ApiException ae = (ApiException) e;
       response.setStatus(ae.getHttpStatus().value());
